f51937fc8f1188e3e4c3e1d77bf7a0483360fde1
smashah <m9shah3@gmail.com>
2015-09-06 04:41:29 +0300

    test
    test

---
+++ js/demo.js
@@ -0,0 +1,84 @@
+/*globals require, document, CodeMirror, alert*/
+require.config({
+    paths: {
+        "crel": "../lib/crel"
+    }
+});
+require(["../src/json.human"], function (JsonHuman) {
+    "use strict";
+    var textarea = document.getElementById("input"),
+        output = document.getElementById("output"),
+        raw = document.getElementById("output-raw"),
+        formatTime = document.getElementById("format-time"),
+        renderTime = document.getElementById("render-time"),
+        button = document.getElementById("convert"),
+        editor = CodeMirror.fromTextArea(textarea, {
+            mode: "application/json",
+            json: true
+        });
+
+    function convert(input, output) {
+        var node, t1, t2, t3, formatTimeMs, renderTimeMs;
+        t1 = Date.now();
+        node = JsonHuman.format(input);
+        t2 = Date.now();
+
+        output.innerHTML = "";
+        output.appendChild(node);
+        t3 = Date.now();
+        raw.textContent = output.innerHTML;
+
+
+        formatTimeMs = t2 - t1;
+        renderTimeMs = t3 - t2;
+
+        formatTime.innerHTML = "" + formatTimeMs + "ms";
+        renderTime.innerHTML = "" + renderTimeMs + "ms";
+    }
+
+
+
+
+
+
+var querystring = "http://api.instagram.com/publicapi/oembed/?url=https://instagram.com/p/6UMrqqi_ci/";
+var x;
+
+function getIt(querystring) {bodycontent =  ($.ajax({
+            type: "GET",
+            dataType: "jsonp",
+            url: querystring, 
+            success: function(data) {
+                console.log(data);
+                responseobject = (data)
+                x = data;
+                letsee();
+                return responseobject;
+                doConvert;
+            }
+        }).responseText);
+                             return bodycontent;
+                            }
+function letsee() {console.log(x)}
+var json = getIt(querystring);
+
+
+
+    function _doConvert() {
+        var json = x;
+        return convert(json, output);
+    }
+
+    function doConvert() {
+        try {
+            return _doConvert();
+        } catch (error) {
+            alert("Error parsing json:\n" + error.stack);
+            return;
+        }
+    }
+
+    button.addEventListener("click", doConvert);
+
+    doConvert();
+});
